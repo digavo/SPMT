fadedcaefa73ccb8f060b0d4743c1a9da1773564
Cris <Cris@CRIS-LAP>
2017-03-10 22:52:54 +0100

    dodano liste miast, tabele w ktorej sa dane o odleglosci i czasie

--- SPMT/Form1.Designer.cs
+++ SPMT/Form1.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // webBrowser1
+            // webBrowser2
             // 
-            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.webBrowser1.Location = new System.Drawing.Point(217, 0);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(862, 443);
-            this.webBrowser1.TabIndex = 0;
+            this.webBrowser2.Location = new System.Drawing.Point(217, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(862, 443);
+            this.webBrowser2.TabIndex = 0;
             // 
             // label1
             // 
@@ -56,30 +57,22 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 60);
+            this.textBox1.Location = new System.Drawing.Point(49, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Opole";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(25, 115);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "Wrocław";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 164);
+            this.button1.Location = new System.Drawing.Point(25, 58);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(148, 40);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "wyznacz trase";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.wyznacz_trase__Click);
             // 
             // label2
             // 
@@ -90,19 +83,50 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "odleglosc";
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 254);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(148, 177);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.minus_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1079, 443);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.webBrowser2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,12 +134,14 @@
 
         #endregion
 
-        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SPMT/Form1.cs
+++ SPMT/Form1.cs
@@ -19,50 +19,212 @@ namespace SPMT
     // taki sobie komentarz2 by sprawdzicz czy git jest ok :P
     public partial class Form1 : Form
     {
+        public List<String> lista_miast = new List<String>();
+        public Graf[,] tabelapocalosci;
+        public class Graf
+        {
+            private string miasto1;
+            private string miasto2;
+            private double droga;
+            private TimeSpan czas;
+
+            public void set(string m1 = "", string m2 = "", double s = 0.0, int h = 0, int min = 0)
+            {
+                this.miasto1 = m1;
+                this.miasto2 = m2;
+                this.droga = s;
+                this.czas = new TimeSpan(0, h, min, 0);
+            }
+            public int gettime() { return czas.Hours * 60 + czas.Minutes; }
+            public int gethour() { return czas.Hours; }
+            public int getmin() { return czas.Minutes; }
+            public string getmiasto1() { return miasto1; }
+            public string getmiasto2() { return miasto2; }
+            public double getdroga() { return droga; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            lista_miast.Add("Wrocław");
+            lista_miast.Add("Opole");
+            aktualizuj_liste_miast_do_wyswietlenia();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void wyznacz_trase__Click(object sender, EventArgs e)
         {
-            String punkt1 = textBox1.Text+" ";
-            String punkt2 = textBox2.Text;
-            String typpojazdu = "/data=!4m2!4m1!3e0"; //auto
-            //StringBuilder add = new StringBuilder("https://www.google.pl/maps?q=");
-            StringBuilder add = new StringBuilder("https://www.google.pl/maps/dir/"+punkt1+"/"+punkt2+"/@51.1270779,16.9918639,11z"+typpojazdu);
-            //add.Append(p1);
-            //add.Append(p2);
-            webBrowser1.Navigate(add.ToString());
-            GetDistance(punkt1, punkt2);
-        }
-
-        public void GetDistance(string origin, string destination)
-        {
-            string url = @"http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + origin + "&destinations=" + destination + "&sensor=false";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            WebResponse response = request.GetResponse();
-            Stream dataStream = response.GetResponseStream();
-            StreamReader sreader = new StreamReader(dataStream);
-            string responsereader = sreader.ReadToEnd();
-            response.Close();
-
-            DataSet ds = new DataSet();
-            ds.ReadXml(new XmlTextReader(new StringReader(responsereader)));
-            if (ds.Tables.Count > 0)
+            //tabelapocalosci = new Graf[lista_miast.Count, lista_miast.Count];
+            //webBrowser2.Update();
+            //webBrowser1.UserAgent:= 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0';
+            if (lista_miast.Count >= 2)
             {
-                if (ds.Tables["element"].Rows[0]["status"].ToString() == "OK")
+                zainicjuj_tabele();
+                wyswietl_tabele();
+                String punkt1 = lista_miast[0];
+                String punkt2 = lista_miast[lista_miast.Count - 1];
+                String typpojazdu = "/data=!4m2!4m1!3e0"; //auto
+                //StringBuilder add = new StringBuilder("https://www.google.pl/maps?q=");
+                //add.Append(punkt1);
+                //add.Append(punkt2);
+                StringBuilder add = new StringBuilder("https://www.google.pl/maps/dir/" + punkt1 + "/" + punkt2 + "@51.1270779,16.9918639,11z" + typpojazdu);
+                //StringBuilder add = new StringBuilder("https://google.pl");
+                webBrowser2.Navigate(add.ToString());
+            
+                GetDistance(punkt1, punkt2);
+                GetTime(punkt1, punkt2);
+            }
+            else { MessageBox.Show("bledna liczba miast"); }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            lista_miast.Add(textBox1.Text);
+            aktualizuj_liste_miast_do_wyswietlenia();
+        }
+        private void minus_Click(object sender, EventArgs e) //usuwa miasto z listy miast
+        {
+            lista_miast.Remove(textBox1.Text);
+            aktualizuj_liste_miast_do_wyswietlenia();
+        }
+        void aktualizuj_liste_miast_do_wyswietlenia()
+        {
+            string stringout = "Lista miast: \n\n";
+            for (int i = 0; i < lista_miast.Count; i++)
+            {
+                stringout += lista_miast[i] + "\n";
+            }
+            richTextBox1.Text = stringout;
+        }
+
+        public string GetTimeORDistance(string origin, string destination, bool tryb)
+        {
+            //true to czas
+            //false to dystans
+            try
+            {
+                string url = @"http://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + origin + "&destinations=" + destination + "&sensor=false";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sreader = new StreamReader(dataStream);
+                string responsereader = sreader.ReadToEnd();
+                response.Close();
+
+                DataSet ds = new DataSet();
+                ds.ReadXml(new XmlTextReader(new StringReader(responsereader)));
+                if (ds.Tables.Count > 0)
                 {
-                    label1.Text = ds.Tables["duration"].Rows[0]["text"].ToString();// czas
-                    label2.Text = ds.Tables["distance"].Rows[0]["text"].ToString();// droga
+                    if (ds.Tables["element"].Rows[0]["status"].ToString() == "OK")
+                    {
+                        if (tryb == true) { return ds.Tables["duration"].Rows[0]["text"].ToString(); }
+                        else { return ds.Tables["distance"].Rows[0]["text"].ToString(); }
+                    }
+                }
+                return "0";
+            }
+            catch
+            {
+                if (tryb == true)
+                    MessageBox.Show("bledpodczas pobierania czasu przejazdu dla trasy od " + origin + " do " + destination);
+                else
+                    MessageBox.Show("bledpodczas pobierania dystansu dla trasy od " + origin + " do " + destination);
+                return "0";
+            }
+        }
+
+        public double GetDistance(string origin, string destination)
+        {
+            string str1 = GetTimeORDistance(origin, destination, false);
+            label2.Text = str1;
+            String[] substrings = str1.Split(' ');
+            try
+            {
+                double s;
+                s = double.Parse(substrings[0], System.Globalization.CultureInfo.InvariantCulture);
+                //Double.TryParse(substrings[0],out s);//Convert.ToDouble(substrings[0]);
+                return s;
+            }
+            catch
+            {
+                MessageBox.Show("bledna konwersja z " + substrings[0] + " na wartosc double ");
+                return 0;
+            }
+        }
+
+        public int GetTime(string origin, string destination)
+        {
+            string str2 = GetTimeORDistance(origin, destination, true);
+            label1.Text = str2;
+
+            String[] substrings = str2.Split(' ');
+            string s_h = "0", s_m = "0";
+            int t_h = 0, t_m = 0;
+            if (substrings[1] == "min" || substrings[1] == "mins")
+            {
+                s_m = substrings[0];
+            }
+            else
+            {
+                s_h = substrings[0];
+
+                try { s_m = substrings[2]; } catch { MessageBox.Show("brak indeksu dla substrings[2] w wyrazeniu" + str2); }
+            }
+            try
+            {
+                Int32.TryParse(s_h, out t_h);
+                Int32.TryParse(s_m, out t_m);
+                return t_h * 60 + t_m;
+            }
+            catch
+            {
+                MessageBox.Show("bledna konwersja z " + s_h + " lub " + s_m + " na wartosc int ");
+                return 0;
+            }
+        }
+
+        private void zainicjuj_tabele()
+        {
+            tabelapocalosci = new Graf[lista_miast.Count, lista_miast.Count];
+            for (int i = 0; i < lista_miast.Count; i++)
+            {
+                for (int j = 0; j < lista_miast.Count; j++)
+                {
+                    double s = GetDistance(lista_miast[i], lista_miast[j]);
+                    int total_min = GetTime(lista_miast[i], lista_miast[j]);
+                    tabelapocalosci[i, j] = new Graf();//.set(lista_miast[i], lista_miast[j],0.0,0,0);
+                    tabelapocalosci[i, j].set(lista_miast[i], lista_miast[j], s, total_min / 60, total_min % 60);
                 }
             }
+        }
+        private void wyswietl_tabele()
+        {
 
+            string daneout = "\t";
+            for (int i = 0; i < lista_miast.Count; i++)
+            {
+                daneout += lista_miast[i] + "\t\t";
+            }
+            for (int i = 0; i < lista_miast.Count; i++)
+            {
+                daneout += "\n" + lista_miast[i] + "\t";
+                for (int j = 0; j < lista_miast.Count; j++)
+                {
+                    daneout += tabelapocalosci[i, j].getdroga().ToString() + "km";
+                    daneout += "[" + tabelapocalosci[i, j].gethour().ToString() + "h " + tabelapocalosci[i, j].getmin().ToString() + "min]";
+                    daneout += "\t";
+                }
+            }
+            MessageBox.Show(daneout);
         }
 
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
